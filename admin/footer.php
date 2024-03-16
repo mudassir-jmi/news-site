28842730abfca9a7471b90c2d92f1efc982d8197
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-16 15:26:51 +0530

    update footer.php

--- admin/footer.php
+++ admin/footer.php
@@ -1,13 +1,11 @@
-<!-- Footer -->
 <div id ="footer">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <span>© Copyright 2019 News | Powered by <a href="http://yahoobaba.net/">Yahoo Baba</a></span>
+                <span>© Copyright 2024 News | Site!</span>
             </div>
         </div>
     </div>
 </div>
-<!-- /Footer -->
 </body>
 </html>
